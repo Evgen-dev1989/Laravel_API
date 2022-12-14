5bd43ccaeeccbce30bca5382f4586edf204c8901
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2022-12-14 20:09:36 +0200

    1
    1

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -4,13 +4,13 @@
         <span class="text-gray-400">({{ $data->count() }})</span>
     </h2>
 
+    @foreach ($data as $date)
     <div class="alert alert-info">
-        @foreach ($data as $date)
             <h3>{{$date->name}}</h3>
             <p>{{$date->email}}</p>
             <p>{{$date->created_at}}</p>
-        @endforeach
     </div>
-    
+    @endforeach
+
 {{$data->links()}}
 @endsection
