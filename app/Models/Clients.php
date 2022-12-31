7237722c0f229225d9805da66bb3e09cd887040f
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2022-12-31 16:38:04 +0200

    2
    2

--- app/Models/Clients.php
+++ app/Models/Clients.php
@@ -2,15 +2,24 @@
 
 namespace App\Models;
 
+use App\Search\Searchable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Clients extends Model
 {
     use HasFactory;
+    use Searchable;
     protected $casts = [
         'tags' => 'json',
     ];
 
+    /**
+     * @return array
+     */
+    public function toElasticsearchDocumentArray(): array
+    {
+        // TODO: Implement toElasticsearchDocumentArray() method.
+    }
 }
 
